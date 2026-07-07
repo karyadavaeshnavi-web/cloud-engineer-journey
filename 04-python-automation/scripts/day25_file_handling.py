4ff4a26956d7cc871eabb8a702e41e9923ac7a27
Karyada Vaeshnavi <karyadavaeshnavi@gmail.com>
2026-07-07 15:54:14 +0530

    Update day25_file_handling.py

---
+++ 04-python-automation/scripts/day25_file_handling.py
@@ -0,0 +1,33 @@
+# Day 25 - File Handling
+
+from datetime import datetime
+
+# Write to hello.txt
+with open("reports/hello.txt", "w") as file:
+    file.write("Hello Cloud Engineer")
+
+# Append to hello.txt
+with open("reports/hello.txt", "a") as file:
+    file.write("\nPython Automation")
+
+# Read hello.txt
+with open("reports/hello.txt", "r") as file:
+    print(file.read())
+
+# Read server list
+print("\n===== SERVERS =====")
+
+with open("reports/servers.txt", "r") as file:
+    for server in file:
+        print(server.strip())
+
+# Create a monitoring report
+with open("reports/server_report.txt", "w") as file:
+
+    file.write("===== SERVER REPORT =====\n")
+    file.write(f"Generated: {datetime.now()}\n")
+    file.write("CPU Usage: 72%\n")
+    file.write("Memory Usage: 65%\n")
+    file.write("Disk Usage: 81%\n")
+
+print("\nServer report generated successfully.")
