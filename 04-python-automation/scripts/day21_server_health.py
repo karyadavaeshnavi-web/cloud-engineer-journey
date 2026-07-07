c4645b88c150cff1f6854712f98200531c02c6b4
Karyadavaeshnavi-web <karyadavaeshnavi@gmail.com>
2026-07-07 15:08:25 +0530

    Complete Day 22 Python Loops

---
+++ 04-python-automation/scripts/day21_server_health.py
@@ -0,0 +1,28 @@
+# Day 21 - Decision Making
+
+server_name = "web01"
+
+cpu = 72
+
+memory = 85
+
+disk = 91
+
+print("===== SERVER HEALTH =====")
+print("Server:", server_name)
+
+if cpu > 80:
+    print("CPU Status : High")
+else:
+    print("CPU Status : Normal")
+
+if memory > 80:
+    print("Memory Status : Warning")
+else:
+    print("Memory Status : Normal")
+
+if disk > 90:
+    print("Disk Status : Critical")
+else:
+    print("Disk Status : Healthy")
+    
